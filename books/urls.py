b2a814c300e8287702a201906dffb7c507fd887d
abhishekY2401 <abhiyd2401@gmail.com>
2024-10-10 01:28:13 +0530

    add routes for templates and apis

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from books.views import add_book, get_books, get_book, update_book, remove_book
+from books.views import *
+
+app_name = 'books'
 
 urlpatterns = [
-    path('add/', add_book, name='add_book'),
-    path('', get_books, name="get_books"),
-    path('<int:id>/', get_book, name='add_book'),
-    path('<int:id>/update', update_book, name='add_book'),
-    path('<int:id>/remove', remove_book, name='add_book')
+    ## template urls ##
+    path('librarian-home/', librarian_home, name='librarian_home'),
+    path('member-home/', member_home, name='member_home'),
+
+    ## api urls ##
+    path('api/books/add/', add_book, name='add_book'),
+    path('api/books/', get_books, name="get_books"),
+    path('api/books/<int:id>/', get_book, name='get_book'),
+    path('api/books/<int:id>/update/', update_book, name='update_book'),
+    path('api/books/<int:id>/remove/', remove_book, name='remove_book'),
+
+    path('api/books/borrow', borrow_book, name='borrow_book'),
+    path('api/books/return', return_book, name='return_book')
 ]
