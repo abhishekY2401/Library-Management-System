abfea136be732c7f0e28eb8a3395e1ee44155e1b
abhishekY2401 <abhiyd2401@gmail.com>
2024-10-10 01:28:27 +0530

    remove prefix from urls

--- library_system/urls.py
+++ library_system/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('users.urls')),
-    path('books/', include('books.urls'))
+    path('', include('users.urls')),
+    path('', include('books.urls'))
 ]
