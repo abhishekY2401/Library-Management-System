d7d74b7de2fbb987c39b14c774517923337f335c
abhishekY2401 <abhiyd2401@gmail.com>
2024-10-09 18:30:16 +0530

    implement views for librarians and members

--- books/views.py
+++ books/views.py
@@ -1,9 +1,12 @@
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from books.models import Book
-from books.serializers import BookSerializer
+from books.models import Book, BookRecord
+from users.models import User
+from books.serializers import BookSerializer, BookRecordSerializer
 from books.permissions import IsLibrarian
+from datetime import timezone
+from django.shortcuts import render
 # Create your views here.
 
 
@@ -20,7 +23,6 @@ def add_book(request):
 
 
 @api_view(["GET"])
-@permission_classes([IsLibrarian])
 def get_books(request):
     try:
         books = Book.objects.all()
@@ -33,7 +35,6 @@ def get_books(request):
 
 
 @api_view(["GET"])
-# @permission_classes([IsAuthenticated])
 def get_book(request, id):
     try:
         book = Book.objects.get(id=id)
@@ -70,3 +71,115 @@ def remove_book(request, id):
         return Response(status=status.HTTP_204_NO_CONTENT)
     except Book.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+@permission_classes([IsLibrarian])
+def view_all_members_records():
+    try:
+        records = BookRecord.objects.all()
+        serializer = BookRecordSerializer(records, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# Member related operations
+
+
+@api_view(['POST'])
+def borrow_books(request):
+    # get the user id and book id for creating a book record of BORROWED status
+    member_id = request.data.get('user_id')
+    book_id = request.data.get('book_id')
+    return_date = request.data.get('return_date')
+
+    try:
+        # now fetch the user and the book data by its corresponding id
+        member = User.objects.get(id=member_id)
+        book = Book.objects.get(id=book_id)
+
+        if member.role == User.Role.LIBRARIAN:
+            return Response({"error": "Only members can borrow books"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if book.status == Book.Status.BORROWED:
+            return Response({"error": "This book is currently not available."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # if available, then assign the book
+        book_record = BookRecord.objects.create(
+            book=book,
+            member=member,
+            action_type=BookRecord.Status.BORROWED,
+            issue_date=timezone.now(),
+            return_date=return_date
+        )
+
+        book.status = Book.Status.BORROWED
+        book.save()
+
+        return Response({"message": "Book borrowed successfully", "book_record": BookRecordSerializer(book_record).data}, status=status.HTTP_201_CREATED),
+
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    except Book.DoesNotExist:
+        return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def return_book(request):
+    # get the user id and book id for creating a book record of BORROWED status
+    member_id = request.data.get('user_id')
+    book_id = request.data.get('book_id')
+
+    try:
+        # now fetch the user and the book data by its corresponding id
+        member = User.objects.get(id=member_id)
+        book = Book.objects.get(id=book_id)
+
+        if member.role == User.Role.LIBRARIAN:
+            return Response({"error": "Only members can return books"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if book.status == Book.Status.AVAILABLE:
+            return Response({"error": "This book is already returned."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # if borrowed, then return the book
+        book_record = BookRecord.objects.create(
+            book=book,
+            member=member,
+            action_type=BookRecord.Status.RETURNED,
+            return_date=timezone.now()
+        )
+
+        book.status = Book.Status.AVAILABLE
+        book.save()
+
+        return Response({'message': 'Book returned successfully', 'book_record': BookRecordSerializer(book_record).data}, status=status.HTTP_201_CREATED)
+
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    except Book.DoesNotExist:
+        return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def get_past_records(request):
+
+    # Get user_id from the request data
+    user_id = request.data.get('user_id')
+
+    if not user_id:
+        return Response({"error": "User ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+
+        records = BookRecord.objects.filter(member=user_id)
+
+        if not records.exists():
+            return Response({"message": "No records found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = BookRecordSerializer(records, many=True)
+        return Response(serializer.data)
+
+    except Exception as error:
+        return Response(f"Error while fetching all records: {str(error)}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
